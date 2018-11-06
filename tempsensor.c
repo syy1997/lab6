962b4c18898c33fe673e4d8a62f2d4561305fc1e
syy1997 <42967670+syy1997@users.noreply.github.com>
2018-11-06 11:29:56 -0500

    Update tempsensor.c

--- tempsensor.c
+++ tempsensor.c
@@ -29,6 +29,7 @@ int main() {
  char* tp3;
   
   while((double)time(NULL)-t1>=1){
+   printf("time>1")
    fp = fopen("/sys/bus/w1/devices/28-021312c86caa/w1_slave.txt", "r");
    if (fp == NULL) {
       puts("cannot open this file");
@@ -67,6 +68,7 @@ int main() {
    
    
   if(ct - prev>=1000){
+   printf("send message")
   ifttt("https://maker.ifttt.com/trigger/temperature_change/with/key/doiyELHkyIVDVYX5-BEg1Y", returnhigher, returnlower, returncurrent); 
   
   }
